3ebcf6875797108d5523a2aed95953023d62b96c
Astrum93 <jungmin2670@gmail.com>
2022-11-08 13:37:21 +0900

    0

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,23 @@ st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYL203CqnnOR5ppB
 # 첫번째 행
 r1_col1, r1_col2, r1_col3 = st.columns(3)
 
-age = r1_col1.number_input("age", step=1, value=23)
+age = r1_col1.number_input("나이", step=1, value=23)
 
-bmi = r1_col2.number_input("bmi", value=34.40)
+bmi = r1_col2.number_input("체질량지수", value=34.40)
 
-children = r1_col3.number_input("children", step=1, value=0)
+children = r1_col3.number_input("자녀", step=1, value=0)
 
 # 두번째 행
 r2_col1, r2_col2, r2_col3 = st.columns(3)
 
-r2_col1.write("smoker")
+r2_col1.write("흡연유무")
 smoker = r2_col1.checkbox("")
 
 sex_option = ("남성", "여성")
 sex = r2_col2.selectbox("성별", sex_option)
 is_male = sex_option[0] == sex
 
-region_option = ('southwest', 'southeast', 'northwest', 'northeast')
+region_option = ('남서쪽', '남동쪽', '북서쪽', '북동쪽')
 region = r2_col3.selectbox("지역", region_option)
 is_southwest = region_option[0] == region
 is_southeast = region_option[1] == region
@@ -43,4 +43,4 @@ if predict_button:
     pred = model.predict(np.array([[age, bmi, children, smoker * 1,
         is_male * 1, is_northwest * 1, is_southeast * 1, is_southwest * 1]]))
 
-    st.metric("예측 보험료", int(pred[0])+'원')
+    st.metric("예측 보험료", map(int(pred[0]))+'원')
