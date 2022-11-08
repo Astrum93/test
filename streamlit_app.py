82fb27f422263f463d5008076598c48b37b7a5b4
Astrum93 <jungmin2670@gmail.com>
2022-11-08 13:58:01 +0900

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ if predict_button:
         is_male * 1, is_northwest * 1, is_southeast * 1, is_southwest * 1]]))
     
 
-    st.metric("예측 보험료",int(pred[0])), st.write("원") 
+    st.metric("예측 보험료",int(pred[0]))
     
